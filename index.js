969f35b7477fb203eae48c979b154865dffbba4d
Chandra Bhandari <rollinrabin@gmail.com>
2025-08-18 23:04:13 +0100

    some changes

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World from Railway!');
 });
+app.get("/universities", (req, res) => {
+  res.json({ message: "Hello from Railway 🚆" });
+});
+
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
+
+
