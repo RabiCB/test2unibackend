9f475f1b1c7bf05c9122d7ae3ea955c732fbf796
Chandra Bhandari <rollinrabin@gmail.com>
2025-09-07 14:47:49 +0100

    feat:cors error solve

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:5173","http://localhost:3000"],
+  origin: ["http://localhost:5173","http://localhost:3000","https://test2uni.com"],
 
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
